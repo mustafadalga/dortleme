545bc8e8b7892ff3921fb7ea91890ad075379119
Mustafa Dalga <mustafadalgaa@gmail.com>
2020-03-07 17:37:24 +0300

    Added FontAwesomeIcon. Prototype variables defined

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueSessionStorage from 'vue-sessionstorage'
+
+Vue.use(VueSessionStorage)
+
 import router from './router'
 
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+
 Vue.config.productionTip = false
+Vue.prototype.notificationIdList = []
+Vue.prototype.notificationCount = 0
+Vue.prototype.currentUser = {}
+
 
 new Vue({
-  router,
-  render: function (h) { return h(App) }
+    router,
+    render: function(h) { return h(App) }
 }).$mount('#app')
