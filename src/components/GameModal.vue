ea9022d1fa38216c29cf6c709e22398fe64f0b6d
Mustafa Dalga <mustafadalgaa@gmail.com>
2020-03-09 00:06:45 +0300

    Dynamic score system was developed.

--- src/components/GameModal.vue
+++ src/components/GameModal.vue
@@ -9,8 +9,41 @@
       tabindex="0"
     >
       <div class="modal-content">
-        <h4 class="center">Oyun Durumu</h4>
-        <p>Kazanan Oyuncu</p>
+        <h4 class="center">
+          <font-awesome-icon :icon="['fas', 'trophy']" class="font-awesome-size teal-text" />
+        </h4>
+
+        <div class="row">
+          <div class="col s12 m6 offset-m3">
+            <div class="card teal darken-1">
+              <div class="card-content white-text center">
+                <span class="card-title">Kazanan Oyuncu</span>
+                <p>{{ kazananOyuncu }}</p>
+              </div>
+              <hr />
+              <div class="card-content white-text center modal-skor-durum">
+                <span class="card-title">Skor Durumu</span>
+                <div class="row">
+                  <div class="col s12 m6">
+                    <p>
+                      <font-awesome-icon :icon="['fas', 'user']" class="font-awesome-size" />
+                    </p>
+                    <p>{{ skor[kazananOyuncuIndex].username}}</p>
+                    <p>{{ skor[kazananOyuncuIndex].puan}}</p>
+                  </div>
+                  <div class="col s12 m6">
+                    <p>
+                      <font-awesome-icon :icon="['fas', 'user']" class="font-awesome-size" />
+                    </p>
+                    <p> {{ skor[kaybedenOyuncuIndex].username }} </p>
+
+                    <p>{{ skor[kaybedenOyuncuIndex].puan}}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div class="modal-footer">
         <button class="btn purple darken-3 waves-green" @click="yeniOyun">Yeni Oyun</button>
@@ -25,20 +58,48 @@
 </template>
 
 <script>
+import { faTrophy, faUser } from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+library.add(faTrophy, faUser);
+
 export default {
+  props: ["kazananOyuncu", "skor"],
   data() {
     return {
-      animatedCSS: true
+      animatedCSS: true,
+      rakip: null,
+      kaybedenOyuncuIndex: null,
+      kazananOyuncuIndex: null
     };
   },
+  created() {
+    this.currentUser = this.$session.get("user");
+    this.rakip = this.$session.get("rakipOyuncu");
+    this.getPlayerIndex();
+  },
+  mounted() {},
   methods: {
+    getPlayerIndex() {
+      this.kazananOyuncuIndex = this.skor.findIndex(player => {
+        return player.username === this.kazananOyuncu;
+      });
+      if (this.kazananOyuncuIndex == 0) {
+        this.kaybedenOyuncuIndex = 1;
+      }else{
+         this.kaybedenOyuncuIndex = 0;
+      }
+    },
     yeniOyun() {
-      this.$emit("yeniOyun");
+      this.animatedCSS = false;
+      setTimeout(() => {
+        this.$emit("yeniOyun");
+      }, 500);
     },
     oyundanCik() {
       this.animatedCSS = false;
       setTimeout(() => {
-        this.$emit("modalClose", false);
+        this.$emit("modalClose");
       }, 500);
       //
     }
@@ -59,7 +120,18 @@ export default {
   display: block;
   opacity: 0.5;
 }
-.btn-red{
-  margin-left:.3em
+.btn-red {
+  margin-left: 0.3em !important;
+}
+.modal-skor-durum .row {
+  margin-bottom: 0px;
+}
+.modal-skor-durum .col {
+  border-bottom: 1px solid #fff;
+  margin-bottom: 1em;
+  padding-bottom: 1em;
+}
+.modal-footer {
+  text-align: center !important;
 }
 </style>
