31b78095d7dca8311785da869eb3d0da1b3028b6
Mustafa Dalga <mustafadalgaa@gmail.com>
2020-03-17 15:17:46 +0300

    Game over actions deficiencies completed

--- src/components/GameOverModal.vue
+++ src/components/GameOverModal.vue
@@ -9,16 +9,18 @@
       tabindex="0"
     >
       <div class="modal-content">
-        <h4 class="center">
+        <h4 class="center" v-if="oyunDurumNo===5">
           <font-awesome-icon :icon="['fas', 'trophy']" class="font-awesome-size teal-text" />
         </h4>
 
-        <div class="row">
+        <div class="row" v-if="oyunDurumNo!==7">
           <div class="col s12 m6 offset-m3">
             <div class="card teal darken-1">
               <div class="card-content white-text center">
-                <span class="card-title">{{ kazananOyuncu ? "Kazanan Oyuncu": "Oyun Berabere Bitti" }}</span>
-                <p>{{ kazananOyuncu ?  (kazananOyuncu == currentUser.email ? currentUser.username : rakip.username) : "" }}</p>
+                <span class="card-title">Oyun Bitti {{ oyunDurumNo }}</span>
+                <p>{{ description }}</p>
+                <p v-if="oyunDurumNo===6">Oyun berabere kaldı.</p>
+                <p v-else>Kazanan Oyuncu: {{kazananOyuncu == currentUser.email ? currentUser.username : rakip.username }}</p>
               </div>
               <hr />
               <div class="card-content white-text center modal-skor-durum">
@@ -29,24 +31,37 @@
                       <font-awesome-icon :icon="['fas', 'user']" class="font-awesome-size" />
                     </p>
                     <p>{{ kazananOyuncu ? (kazananOyuncu == currentUser.email ? currentUser.username : rakip.username) : currentUser.username }}</p>
-                    <p>{{ kazananOyuncu ? (kazananOyuncu == currentUser.email ? currentUserSkor : rakipSkor) :currentUserSkor }}</p>
+                    <p>{{kazananOyuncu ? (kazananOyuncu == currentUser.email ? currentUserSkor : rakipSkor) :currentUserSkor }}</p>
                   </div>
                   <div class="col s12 m6">
                     <p>
                       <font-awesome-icon :icon="['fas', 'user']" class="font-awesome-size" />
                     </p>
-                    <p>{{  kazananOyuncu ? (kazananOyuncu == currentUser.email ? rakip.username : currentUser.username) : rakip.username }}</p>
-                  <p>{{ kazananOyuncu ? (kazananOyuncu == currentUser.email ? rakipSkor : currentUserSkor) : rakipSkor }}</p>
-
+                    <p>{{ kazananOyuncu ? (kazananOyuncu == currentUser.email ? rakip.username : currentUser.username) : rakip.username }}</p>
+                    <p>{{ kazananOyuncu ? (kazananOyuncu == currentUser.email ? rakipSkor : currentUserSkor) : rakipSkor }}</p>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
+        <div class="row" v-else>
+          <div class="col s12 m6 offset-m3">
+            <div class="card teal darken-1">
+              <div class="card-content white-text center">
+                <span class="card-title">Oyun Bitti</span>
+                <p>{{ rakip.username+" oyundan çıkış yaptı." }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div class="modal-footer">
-        <button class="btn purple darken-3 waves-green" @click="yeniOyun">Yeni Oyun</button>
+        <button
+          v-if="oyunDurumNo===5 || oyunDurumNo===6"
+          class="btn purple darken-3 waves-green"
+          @click="yeniOyun"
+        >Yeni Oyun</button>
         <button class="btn red btn-red" @click="oyundanCik">Oyundan Çık</button>
       </div>
     </div>
@@ -65,18 +80,31 @@ library.add(faTrophy, faUser);
 
 export default {
   name: "GameOverModal",
-  props: ["kazananOyuncu", "currentUserSkor", "rakipSkor"],
+  props: ["kazananOyuncu", "currentUserSkor", "rakipSkor", "oyunDurumNo"],
   data() {
     return {
       animatedCSS: true,
       rakip: null,
       kaybedenOyuncuIndex: null,
-      kazananOyuncuIndex: null
+      kazananOyuncuIndex: null,
+      description: null
     };
   },
   created() {
     this.currentUser = this.$session.get("user");
     this.rakip = this.$session.get("rakipOyuncu");
+    if (this.oyunDurumNo === 3) {
+      this.description =
+        "Rakip oyuncu " +
+        this.rakip.username +
+        " oyundan çıkış yaptığın için hükmen mağlup oldu.";
+    } else if (this.oyunDurumNo === 4) {
+      this.description =
+        "1 dakikalık bekleme süresi sonucunda rakip oyuncu " +
+        this.rakip.username +
+        " oyuna giriş yapmadığından dolayı hükmen mağlup oldu";
+    }
+     
   },
   methods: {
     yeniOyun() {
@@ -88,7 +116,7 @@ export default {
     oyundanCik() {
       this.animatedCSS = false;
       setTimeout(() => {
-        this.$emit("oyunCikis", this.$options.name);
+        this.$emit("oyunCikis");
       }, 500);
     }
   }
