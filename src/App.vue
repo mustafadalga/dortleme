e94981b7abf240b55a1e3378a706e6497b509a73
Mustafa Dalga <mustafadalgaa@gmail.com>
2020-03-20 14:19:41 +0300

    Added watch for dynamically change background color

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,33 @@
 <template>
   <div id="app">
-
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
+
+<script>
+export default {
+  mounted() {
+    if (this.$route.name === "Game") {
+      document.body.style.background = "none";
+      document.getElementById("root").style.backgroundImage =
+        "url('assets/img/zemin.png')";
+    } else {
+      document.body.style.background = "#ede7f6";
+      document.getElementById("root").style.backgroundImage = "none";
+    }
+  },
+  watch: {
+    $route(to) {
+      if (to.name === "Game") {
+        document.body.style.background = "none";
+        document.getElementById("root").style.backgroundImage =
+          "url('assets/img/zemin.png')";
+      } else {
+        document.body.style.background = "#ede7f6";
+        document.getElementById("root").style.backgroundImage = "none";
+      }
+    }
+  }
+};
+</script>
