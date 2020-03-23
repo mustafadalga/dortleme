f7f9e91cd7bd4030e27f1df3f88a986cd387a17f
Mustafa Dalga <mustafadalgaa@gmail.com>
2020-03-24 01:15:31 +0300

    Added vue-notification package

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueSessionStorage from 'vue-sessionstorage'
-
+import Notifications from 'vue-notification'
 Vue.use(VueSessionStorage)
+Vue.use(Notifications)
 
 import router from './router'
 
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-
 
 Vue.config.productionTip = false
 Vue.prototype.notificationIdList = []
