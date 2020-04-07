c1ca453b733b0566b0a1cd1a6f042c9952e40c9c
Mustafa Dalga <mustafadalgaa@gmail.com>
2020-04-07 21:58:34 +0300

    Added changeBodyCSS method

--- src/App.vue
+++ src/App.vue
@@ -7,49 +7,51 @@
 
 <script>
 export default {
+  data() {
+    return {
+      routeName: null
+    };
+  },
   mounted() {
-    if (this.$route.name === "Game") {
-      document.body.style.background = "none";
-      document.getElementById("root").style.backgroundImage =
-        "url('assets/img/zemin.png')";
-    } else {
-      document.body.style.background = "#ede7f6";
-      document.getElementById("root").style.backgroundImage = "none";
-    }
+    this.changeBodyCSS();
   },
   watch: {
     $route(to) {
+      this.routeName = to.name;
       this.changeTitle();
-      if (to.name === "Game") {
-        document.body.style.background = "none";
-        document.getElementById("root").style.backgroundImage =
-          "url('assets/img/zemin.png')";
-      } else {
-        document.body.style.background = "#ede7f6";
-        document.getElementById("root").style.backgroundImage = "none";
-      }
+      this.changeBodyCSS(to.name);
     }
   },
   methods: {
     changeTitle() {
-      let routeName = this.$route.name;
-      let title = "Connection 4 | ";
-      if (routeName === "Signup") {
+      let title = "Dörtleme | ";
+      if (this.routeName === "Signup") {
         title += "Kayıt Ol";
-      } else if (routeName === "Login") {
+      } else if (this.routeName === "Login") {
         title += "Giriş Yap";
-      } else if (routeName === "ResetPassword") {
+      } else if (this.routeName === "ResetPassword") {
         title += "Parolamı Unuttum";
-      } else if (routeName === "Home") {
+      } else if (this.routeName === "Home") {
         title += "Anasayfa";
-      } else if (routeName === "Notifications") {
+      } else if (this.routeName === "Notifications") {
         title += "Bildirimler";
-      } else if (routeName === "Ratings") {
+      } else if (this.routeName === "Ratings") {
         title += "Skor Durumu";
-      } else if (routeName === "Game") {
+      } else if (this.routeName === "Game") {
         title += "Oyun";
       }
       document.title = title;
+    },
+    changeBodyCSS() {
+      if (this.routeName === "Game") {
+        document.body.style.height = "100vh";
+        document.body.style.background = "none";
+        document.getElementById("root").style.backgroundImage ="url('assets/img/zemin.png')";
+      } else {
+        document.body.style.height = "auto";
+        document.body.style.background = "#ede7f6";
+        document.getElementById("root").style.backgroundImage = "none";
+      }
     }
   }
 };
